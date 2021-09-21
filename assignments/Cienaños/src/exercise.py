87fffbfd23b533a6a883a0b7c3155e10bf9fb260
JuannAraiza <a01740671@itesm.mx>
2021-09-21 13:43:17 +0000

    resuelto 100 años

---
+++ assignments/Cienaños/src/exercise.py
@@ -0,0 +1,11 @@
+def main():
+    #escribe tu código abajo de esta línea
+    Edad = int(input('>>>'))
+    agno = int(input('>>>'))
+
+    ango100 = (agno-Edad)+100
+
+    print(ango100)
+
+if __name__=='__main__':
+    main()
